735e4d2e8a0a4e17a8dd11601b586604118c69ea
TenSt <stepan.maks@gmail.com>
2019-10-13 02:31:15 +0300

    clean-up

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,22 +10,15 @@ fi
 git config --global user.name $3
 git config --global user.email $3
 
-echo "go to /opt"
 cd /opt
-echo "creating new folder"
 mkdir mnist-serve
-echo "go to new folder"
 cd ./mnist-serve
-echo $1
-echo $2
-echo "cloning repo"
 git clone https://$2 
 cd mnist-serve
 ls -lsa
 rm -rf ./export
 cp -r /mnt/export ./
 rm -rf /mnt/export
-echo "mnist-serve" > README.md
 ls -lsa
 git add .
 git commit -m "adding new version of the model to serve with sha $5"
